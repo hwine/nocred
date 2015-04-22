06f10a973c84ae194e4912e73201c4da1287fb8a
Hal Wine <hwine@mozilla.com>
2015-04-22 14:08:45 -0700

    tests pass! :)

--- nocredentials/__main__.py
+++ nocredentials/__main__.py
@@ -5,6 +5,19 @@ import sys
 # - https://docs.python.org/2/using/cmdline.html#cmdoption-m
 # - https://docs.python.org/3/using/cmdline.html#cmdoption-m
 
+import logging
+import argparse
+import fileinput
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args(argv=()):
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("file", help='file to scan',
+                        nargs='+')
+    return parser.parse_args(argv)
+
 
 def main(argv=()):
     """
@@ -17,8 +30,11 @@ def main(argv=()):
     Does stuff.
     """
 
-    print(argv)
+    args = parse_args(argv)
+    args
+    # assume success if we reached here
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main())
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+    sys.exit(main(sys.argv[1:]))
--- tests/test_nocredentials.py
+++ tests/test_nocredentials.py
@@ -5,7 +5,26 @@ from nocredentials.__main__ import main
 
 class MainTestCase(unittest.TestCase):
     def test_main(self):
-        self.assertEqual(main([]), 0)
+        with self.assertRaises(SystemExit) as cm:
+            main([])
+        self.assertEqual(cm.exception.code, 2)
+
+
+class ArgTestCase(unittest.TestCase):
+    def test_help(self):
+        with self.assertRaises(SystemExit) as cm:
+            main(['-h'])
+        self.assertEqual(cm.exception.code, 0)
+
+    def test_help_long(self):
+        with self.assertRaises(SystemExit) as cm:
+            main(['--help'])
+        self.assertEqual(cm.exception.code, 0)
+
+    def test_bad_arg(self):
+        with self.assertRaises(SystemExit) as cm:
+            main(['-X'])
+        self.assertEqual(cm.exception.code, 2)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(buffer=True)
